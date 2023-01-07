ea062b5bca150089fe8b26cb5fa38e31bd807e38
U-COMET\Mr. Timn Tina Blue <tj767@live.ca>
2023-01-07 03:30:38 -0800

    adding my keymap

---
+++ keyboards/splitkb/kyria/keymaps/AliferousWolf/rules.mk
@@ -0,0 +1,9 @@
+
+
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+WPM_ENABLE = yes		# Enables the words per minute calculation
+#SPLIT_KEYBOARD = yes	# Enable the use of split keyboard define in config.h
